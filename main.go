bafba1bb9d8b065692245a7ae94f800d50f8061c
Busiso Chisala <busiso.chisala@gmail.com>
2016-08-21 12:16:37 +0100

    moving towards cleaner code ...

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 /*
 	localhost:8000
 	generates a *color* 1024x1024 mandelbrot progressively
-	modify
+	numberOfroutinesify
 	refresh rate (r),
 	iterations (num),
 	square size (w),
@@ -44,7 +44,7 @@ func main() {
 // display the base html
 func handler_init(w http.ResponseWriter, r *http.Request) {
 	setCoords()
-	getPars(r) // handle the querystring
+	ReadQueryString(r) // handle the querystring
 	// note the use of backtick!!
 	w.Write([]byte(`
 <html>
@@ -74,8 +74,8 @@ clr: <input type="text" size=3 name="col">
 
 // return a mandelbrot image
 func handler_image(w http.ResponseWriter, r *http.Request) {
-	getPars(r)  // handle the querystring
-	getImage(w) // fetch the base64 encoded png image
+	ReadQueryString(r)  // handle the querystring
+	SendImage(w) // fetch the base64 encoded png image
 }
 
 const (
@@ -90,7 +90,7 @@ var (
 	position    = 0
 	rate        = 128               // how quickly we process - default if we dont  ?r=345 etc
 	num         = 1                 // multiples of 600
-	Mod         = 4                 // number of concurrent go routines
+	numberOfroutines         = 4                 // number of concurrent go routines
 	cx, cy, rad = -0.73, 0.23, 0.02 // x0=cx-rad, y0=cy-rad
 	col         = 1256.0            // color
 	options     = map[string]int{
@@ -116,7 +116,7 @@ func setCoords() {
 	position = 0
 }
 
-func getPars(r *http.Request) {
+func ReadQueryString(r *http.Request) {
 	if err := r.ParseForm(); err != nil {
 		log.Print(err)
 	}
@@ -150,7 +150,7 @@ func getPars(r *http.Request) {
 		case "r":
 			rate = n // global var rate
 		case "m":
-			Mod = n // global var Mod - number of goroutines
+			numberOfroutines = n // global var number of goroutines
 		case "x":
 			cx = z // global var center x coord
 		case "y":
@@ -183,53 +183,43 @@ func getPars(r *http.Request) {
 	}
 }
 
-func getImage(w io.Writer) {
-	if position == N { // complete we are done, send this indicator to js
-		sx := strconv.FormatFloat(cx, 'f', -1, 64)
-		sy := strconv.FormatFloat(cy, 'f', -1, 64)
-		sr := strconv.FormatFloat(rad, 'f', -1, 64)
-		si := strconv.Itoa(num)
-		st := strconv.Itoa(rate)
-		sm := strconv.Itoa(Mod)
-		sc := strconv.FormatFloat(col, 'f', -1, 64)
-		io.WriteString(w, "_"+sx+"_"+sy+"_"+sr+"_"+si+"_"+st+"_"+sm+"_"+sc)
+
+func SendImage(w io.Writer) {
+	if position == N { // complete we are done, send this banner to js
+		SendBanner(w)
 		position = 0
 		return
 	}
-	if rate == 0 {
-		rate = 256
-	}
-	lim := position + 1024*rate
-	fmt.Print("Rate = ", rate, ", position = ", position, "\n")
-	//fmt.Print("cx = ",cx,", cy = ",cy,", rad = ",rad,"\n")
-	if lim > N {
-		lim = N
+
+	lastIndex := stopAt(rate,position)
+	
+	screenChan := make(chan image.Image)
+
+	// set the partial image go routines going
+	for i := 0; i < numberOfroutines; i++ {
+		go BuildImage(lastIndex, numberOfroutines, i, screenChan)
 	}
 
-	buf := new(bytes.Buffer)
-	c := make(chan image.Image)
-
+	// assemble the image
 	img0 := image.NewRGBA(image.Rect(0, 0, width, height))
-
-	for i := 0; i < Mod; i++ {
-		go partialImage(lim, Mod, i, c)
-	}
-
 	count := 0
 	op := draw.Src
-	for img := range c {
+	for img := range screenChan {
 		draw.Draw(img0, img0.Bounds(), img, image.ZP, op)
-		if op == draw.Src {
+		if op == draw.Src { // switch the drawing operation one time
 			op = draw.Over
-		} // swicth the one time
+		} 
 		count++
-		if count == Mod {
-			close(c)
+		if count == numberOfroutines {
+			close(screenChan)
 		}
 	}
 
+	// the assembly loop had blocked, and now we process result for writer w
+	position = lastIndex                   // update starting point for next partial
+
+	buf := new(bytes.Buffer)
 	png.Encode(io.Writer(buf), img0) // NOTE: ignoring errors, to an io.Writer
-	position = lim                   // update
 
 	// convert to base64
 	encoder := base64.NewEncoder(base64.StdEncoding, w) // send to target w
@@ -237,37 +227,78 @@ func getImage(w io.Writer) {
 	encoder.Close()
 }
 
-func partialImage(lim int, base int, part int, c chan image.Image) {
+func stopAt(rate, position int) int {
+	if rate == 0 {
+		rate = 256
+	}
+	lastInd := position + 1024*rate
+	fmt.Print("Rate = ", rate, ", position = ", position, "\n")
+	//fmt.Print("cx = ",cx,", cy = ",cy,", rad = ",rad,"\n")
+	if lastInd > N {
+		lastInd = N
+	}
+	return lastInd	
+}
+
+type Point struct{
+	Right int	// right from TL corner (0,0)
+	Down int	// down from ""
+}
+
+// BuildImage generates a partial image of the Mandelbrot set and sends 
+// this to screenChan
+func BuildImage(lastIndex int, base int, part int, screenChan chan image.Image) {
 	img := image.NewRGBA(image.Rect(0, 0, width, height))
 	draw.Draw(img, img.Bounds(), image.Transparent, image.ZP, draw.Src)
 
-	var (
-		px, py, r_k  int     //pixel positions
-		rx, ry       float64 //real, im parts
-		x0, y0, side float64 // -0.75, 0.21, 0.04
-		z            complex128
-	)
-
-	x0 = cx - rad
-	y0 = cy - rad
-	side = 2 * rad
-
-	for k := position; k < lim; k++ {
+	for k := position; k < lastIndex; k++ {
 		if k%base != part {
 			continue
 		} // choose our residue class
-		r_k = perm[k] // randomize
-		px = x[r_k]
-		py = y[r_k]
-		rx = x0 + float64(px)/width*side
-		ry = y0 + float64(py)/height*side
-		z = complex(rx, ry)
-		img.Set(px, height-py, mandelbrot(z)) //orientation: '-'
+		P := NextPoint(k)
+		z := Point2C(P)
+		img.Set(P.Right, height-P.Down, GetColor(z))
 	}
-	c <- img
+	screenChan <- img
 }
 
-func mandelbrot(z complex128) color.Color {
+// SendBanner writes the details of the last build
+// at the top of the page
+func SendBanner(w io.Writer) {
+	sx := strconv.FormatFloat(cx, 'f', -1, 64)
+	sy := strconv.FormatFloat(cy, 'f', -1, 64)
+	sr := strconv.FormatFloat(rad, 'f', -1, 64)
+	si := strconv.Itoa(num)
+	st := strconv.Itoa(rate)
+	sm := strconv.Itoa(numberOfroutines)
+	sc := strconv.FormatFloat(col, 'f', -1, 64)
+	io.WriteString(w, "_"+sx+"_"+sy+"_"+sr+"_"+si+"_"+st+"_"+sm+"_"+sc)
+}
+
+// NextPoint returns the point on the screen
+// parametrized by k, using our randomization
+func NextPoint(k int) Point {
+		rk := perm[k] // randomize
+		return Point{x[rk], y[rk]}
+}
+
+// P2C converts a pixel point Q to
+// a complex number
+func Point2C(Q Point) complex128 {
+	px := Q.Right
+	py := Q.Down
+	side := 2 * rad
+	rx := cx - rad + float64(px)/width*side
+	ry := cy - rad + float64(py)/height*side
+	return complex(rx, ry)
+}
+
+
+// GetColor runs the Mandelbrot iteration from point z
+// it returns a color for the point dependent on how many
+// iterations are required to escape. If this is more than
+// our bound we assign color.Black
+func GetColor(z complex128) color.Color {
 	var (
 		c uint64
 		v complex128
