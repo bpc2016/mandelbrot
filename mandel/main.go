d53dcadbb53956733f069f6d96e31641b2baca88
Busiso Chisala <busiso.chisala@gmail.com>
2016-08-24 11:24:02 +0100

    more cleanups

--- mandel/main.go
+++ mandel/main.go
@@ -20,6 +20,32 @@ import (
 	"strings"
 )
 
+const (
+	width, height = 1024, 512
+	N             = width * height // number of pixels
+	tookTooLong   = 0              // flag failure, color Black
+)
+
+type Point struct {
+	Right int // right from TL corner (0,0)
+	Down  int // down from ""
+}
+
+var (
+	Color            func(d int) color.RGBA
+	Sigma            func(d int) int
+	Px2S             func(pr, pd int) (float64, float64) // pixel to Cartesian
+	Px2C             func(p Point) complex128
+	Pixel            [N]Point // all the screen points
+	position         = 0
+	rate             = 32          // how quickly we process - default if we dont  ?r=345 etc
+	iterations       = 3000        //
+	numberOfroutines = 2           // number of concurrent go routines
+	cx, cy           = -0.717, 0.23 // x, y coords of central point pixel (width/2,height/2)
+	hScale           = 0.02        // hScale = cx-0
+	density          = 8          // color
+)
+
 func main() {
 	http.HandleFunc("/", handler_init)
 	http.HandleFunc("/image/", serveImage)
@@ -28,33 +54,15 @@ func main() {
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
 
-// display the base html
-func handler_init(w http.ResponseWriter, r *http.Request) {
-	ReadQueryString(r) // handle the querystring
-	// set the pixel to point mapping
-	Px2S = math.Transformation(cx, cy, hScale, width, height)
-	Px2C = func(p Point) complex128 {
-		re, im := Px2S(p.Right, p.Down)
-		return complex(re, im)
-	}
-	w.Write([]byte(html.UI))
-}
-
-// return a Mandelbrot image
-func serveImage(w http.ResponseWriter, r *http.Request) {
-	getImageReq(r)
-	//ReadQueryString(r) // handle the querystring
-	SendImage(w)       // fetch the base64 encoded png image
-}
-
+// SendImage is the heart of this program. It renders the (partial)
+// image to send to the http server, signalling completion
+// by sending a banner
 func SendImage(w io.Writer) {
 	if position == N { // complete we are done, send this banner to js
-		SendBanner(w)
+		io.WriteString(w,Banner())
 		position = 0
 		return
 	}
-
-	iterations = num * 600 // we scale the given iterations
 
 	lastIndex := resetEnd(position, rate)
 
@@ -88,148 +96,6 @@ func SendImage(w io.Writer) {
 	position = lastIndex // update starting point for next partial
 }
 
-
-var visited bool
-
-func getImageReq(r *http.Request) {
-	var err error
-	if err = r.ParseForm(); err != nil {
-		log.Print(err)
-	}
-
-	//set up our vars num=iterations
-	for k, v := range r.Form {
-		if options[k] != 3 {
-			continue
-		}
-		if position == 0 {
-			visited = false
-		}
-
-		if !visited {
-			var x1, y1 int
-			
-			if k == "newpt" { // move center
-				w := strings.Split(v[0], "|")
-				x1, err = strconv.Atoi(w[0])
-				if err != nil {
-					log.Print(err)
-					continue
-				}
-				y1, err = strconv.Atoi(w[1])
-				if err != nil {
-					log.Print(err)
-					continue
-				}
-				// recenter
-				cx,cy = Px2S(x1,y1)
-			}
-
-			if k == "in" { // move center
-		 		hScale = hScale * 3 / 4
-			}
-
-			if k == "out" { // move center
-		 		hScale = hScale * 2 
-			}
-
-			// ini all these cases, reset
-			Px2S = math.Transformation(cx, cy, hScale, width, height)
-			Px2C = func(p Point) complex128 {
-				re, im := Px2S(p.Right, p.Down)
-				return complex(re, im)
-			}
-			visited = true
-		}
-	}
-
-}
-
-const (
-	width, height = 1024, 512
-	N             = width * height // number of pixels
-	tookTooLong   = 0              // flag failure, color Black
-)
-
-type Point struct {
-	Right int // right from TL corner (0,0)
-	Down  int // down from ""
-}
-
-var (
-	Color            func(d int) color.RGBA
-	Sigma            func(d int) int
-	Px2S             func(pr, pd int) (float64, float64) // pixel to Cartesian
-	Px2C             func(p Point) complex128
-	Pixel            [N]Point // all the screen points
-	position         = 0
-	rate             = 64          // how quickly we process - default if we dont  ?r=345 etc
-	num              = 5           // multiples of 600
-	iterations       = 3000        //  always 600 x  TODO kill one of these two
-	numberOfroutines = 2           // number of concurrent go routines
-	cx, cy           = -0.73, 0.23 // x, y coords of central point pixel (width/2,height/2)
-	hScale           = 0.02        // hScale = cx-0
-	col              = 1256.0      // color
-	options          = map[string]int{
-		"m": 1, "r": 1, "num": 1, // 1 = int, 2 = float
-		"x": 2, "y": 2, "w": 2,
-		"dpx": 1, "dpy": 1,
-		"in": 3, "out": 3,
-		"col": 2, "newpt": 3,
-	}
-)
-
-func ReadQueryString(r *http.Request) {
-	if err := r.ParseForm(); err != nil {
-		log.Print(err)
-	}
-
-	//set up our vars num=iterations
-	for k, v := range r.Form {
-		if options[k] == 0 {
-			continue
-		}
-		if options[k] == 3 {
-			continue
-		}
-		var (
-			n   int
-			z   float64
-			err error
-		)
-		if options[k] == 1 { // int value
-			n, err = strconv.Atoi(v[0])
-			if err != nil {
-				log.Print(err)
-				continue
-			}
-		} else {
-			z, err = strconv.ParseFloat(v[0], 64)
-			if err != nil {
-				log.Print(err)
-				continue
-			}
-		}
-		switch k {
-		case "num":
-			num = n // global var number of  iterations
-		case "r":
-			rate = n // global var rate
-		case "m":
-			numberOfroutines = n // global var number of goroutines
-		case "x":
-			cx = z // global var center x coord
-		case "y":
-			cy = z // global var center y coord
-		case "w":
-			hScale = z // global var half a side
-		case "col":
-			col = z // change the hue
-		}
-	}
-}
-
-
 // Encode take image, first PNG encodes it then
 // writes its Base64 encoding to writer w
 func Encode(w io.Writer, image *image.RGBA) {
@@ -242,19 +108,7 @@ func Encode(w io.Writer, image *image.RGBA) {
 	encoder.Close()
 }
 
-// resetEnd returns the last index for points given
-// the 'rate' refresh (which decides refresh) and
-// current index pos along points list
-func resetEnd(pos, refresh int) int {
-	if refresh == 0 {
-		refresh = 256
-	}
-	lastInd := pos + 1024*refresh
-	if lastInd > N {
-		lastInd = N
-	}
-	return lastInd
-}
+
 
 // BuildImage generates a partial image of the Mandelbrot set and sends
 // this to screenChan. This is called in a goroutine indexed by part <= numberOfgoroutines
@@ -271,7 +125,7 @@ func BuildImage(part int, lastIndex int, screenChan chan image.Image) {
 		p := Pixel[Sigma(k)] // use our permutation
 		z := Px2C(p)         //Point2C(p)
 		d := mandelBrot(z)
-		img.Set(p.Right, p.Down, Color(18*d)) // no switch??
+		img.Set(p.Right, p.Down, Color(density*d)) // no switch??
 	}
 
 	screenChan <- img
@@ -282,7 +136,7 @@ func BuildImage(part int, lastIndex int, screenChan chan image.Image) {
 // tookTooLong (=0)
 func mandelBrot(z complex128) int {
 	var v complex128
-	for n := 0; n < iterations; n++ { // iterations comes from global 'num' !!
+	for n := 0; n < iterations; n++ {
 		v = v*v + z
 		if cmplx.Abs(v) > 2 {
 			return n
@@ -291,20 +145,29 @@ func mandelBrot(z complex128) int {
 	return tookTooLong
 }
 
-// SendBanner writes the details of the last build
-// at the top of the page
-func SendBanner(w io.Writer) {
-	sx := strconv.FormatFloat(cx, 'f', -1, 64)
-	sy := strconv.FormatFloat(cy, 'f', -1, 64)
-	sr := strconv.FormatFloat(hScale, 'f', -1, 64)
-	si := strconv.Itoa(num)
-	st := strconv.Itoa(rate)
-	sm := strconv.Itoa(numberOfroutines)
-	sc := strconv.FormatFloat(col, 'f', -1, 64)
-	// the leading '_' below is a signal to the client that we are finished (see html.UI, js)
-	io.WriteString(w, "_"+sx+"_"+sy+"_"+sr+"_"+si+"_"+st+"_"+sm+"_"+sc)
+// resetEnd returns the last index for points given
+// the 'rate' refresh (which decides refresh) and
+// current index pos along points list
+func resetEnd(pos, refresh int) int {
+	if refresh == 0 {
+		refresh = 32
+	}
+	lastInd := pos + 1024*refresh
+	if lastInd > N {
+		lastInd = N
+	}
+	return lastInd
 }
 
+func getTransforms() {
+	Px2S = math.Transformation(cx, cy, hScale, width, height)
+	Px2C = func(p Point) complex128 {
+		re, im := Px2S(p.Right, p.Down)
+		return complex(re, im)
+	}
+}
+
+//========================================== initialize ======================================================
 
 func init() {
 	k := 0
@@ -315,13 +178,7 @@ func init() {
 		}
 	}
 
-	permutation := math.RandPermutation(N)
-	Sigma = func(d int) int {
-		if d < 0 || d >= N {
-			panic(fmt.Sprintf("can't handle % in permutation", d))
-		}
-		return permutation[d]
-	}
+	Sigma = math.RandPermFunc(N) // we walk through pixels with this
 
 	position = 0 // for partial image creation
 
@@ -332,5 +189,144 @@ func init() {
 		}
 		return Rainbow[d%1530] // len(Rainbow) = 255*6
 	}
+
 	fmt.Println("MandelBrot server started on  http://localhost:8000")
+}
+
+//======================================= ui stuff ================================================
+
+// display the base html
+func handler_init(w http.ResponseWriter, r *http.Request) {
+	ReadQueryString(r) // handle the querystring
+	// set the pixel to point mapping
+	getTransforms()
+	w.Write([]byte(html.UI))
+}
+
+// return a Mandelbrot image
+func serveImage(w http.ResponseWriter, r *http.Request) {
+	getImageReq(r) // handle the querystring
+	SendImage(w)   // fetch the base64 encoded png image
+}
+
+var visited bool
+
+// getImageReq handles http requests from
+// user input: click and keyboard
+// uses 'visited' above so as not to repeat itself
+func getImageReq(r *http.Request) {
+	checkF(r.ParseForm())
+
+	var err error
+	for k, v := range r.Form {
+		if !(k == "newpt" || k == "in" || k == "out") {
+			continue
+		}
+
+		visited = position != 0
+
+		if !visited {
+			var newr, newd int
+
+			if k == "newpt" { // center data: pr|pd
+				w := strings.Split(v[0], "|")
+				newr, err = strconv.Atoi(w[0])
+				if err != nil { // just use previous value
+					log.Printf("format error for %v : %v", k, err)
+					continue
+				}
+				newd, err = strconv.Atoi(w[1])
+				if err != nil { // just use previous value
+					log.Printf("format error for %v : %v", k, err)
+					continue
+				}
+				// recenter
+				cx, cy = Px2S(newr, newd)
+			}
+
+			if k == "in" { // scale in
+				hScale = hScale * 3 / 4
+			}
+
+			if k == "out" { // scale out
+				hScale = hScale * 2
+			}
+			// in all these cases, reset
+			getTransforms()
+			visited = true
+		}
+	}
+
+}
+
+var numberType = map[string]int{
+	"m": 1, "r": 1, "num": 1, // 1 = int, 2 = float
+	"x": 2, "y": 2, "w": 2,
+	"dpx": 1, "dpy": 1,
+	"col": 1,
+}
+
+func ReadQueryString(r *http.Request) {
+	// read the form
+	checkF(r.ParseForm())
+
+	//set up our vars
+	for k, v := range r.Form {
+		if numberType[k] == 0 {
+			continue
+		}
+		var (
+			n   int
+			z   float64
+			err error
+		)
+		if numberType[k] == 1 { // int value
+			n, err = strconv.Atoi(v[0])
+		} else {
+			z, err = strconv.ParseFloat(v[0], 64)
+		}
+		if err != nil { // just use previous value
+			log.Printf("format error for %v : %v", k, err)
+			continue
+		}
+		switch k {
+		case "num":
+			iterations = n // global var number of  iterations
+		case "r":
+			rate = n // global var rate
+		case "m":
+			numberOfroutines = n // global var number of goroutines
+		case "x":
+			cx = z // global var center x coord
+		case "y":
+			cy = z // global var center y coord
+		case "w":
+			hScale = z // global var half a side
+		case "col":
+			density = n // change the hue
+		}
+	}
+}
+
+// Banner writes the details of the last build
+// at the top of the page
+func Banner() string {
+	sx := strconv.FormatFloat(cx, 'f', -1, 64)
+	sy := strconv.FormatFloat(cy, 'f', -1, 64)
+	sr := strconv.FormatFloat(hScale, 'f', -1, 64)
+	si := strconv.Itoa(iterations)
+	st := strconv.Itoa(rate)
+	sm := strconv.Itoa(numberOfroutines)
+	sc := strconv.Itoa(density)
+	// the leading '_' below is a signal to the client that we are finished (see html.UI, js)
+	return "_"+sx+"_"+sy+"_"+sr+"_"+si+"_"+st+"_"+sm+"_"+sc
+}
+
+//======================= utility ================================
+
+// abort on errors
+func checkF(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
 }
--- rgba/rgba.go
+++ rgba/rgba.go
@@ -3,34 +3,10 @@
 package rgba
 
 import (
-	//"fmt"
 	"image/color"
-	"math"
+	"mandelbrot/math"
 )
 
-var iterations int
-var f int
-var col float64
-
-// SetPalette simply passes us these two data
-func SetPalette(num int, thecol float64) {
-	iterations = num * 600 // TODO - get rid of this one , identify with iterations
-	const M = 16777215     // 255,255,255 (white)
-	f = int(math.Floor(float64(M) / float64(iterations)))
-	// fmt.Println("FACTOR: ",f)
-	col = thecol
-}
-
-// PxColor converts integer duration to a color
-// according to our palette
-func PxColor0(duration int) color.Color {
-	if duration == 0 { // tookTooLong
-		return color.Black
-	}
-	c := coloRatio(duration)
-	w := getColors(c)
-	return color.RGBA{w[0], w[1], w[2], 255}
-}
 
 /*
 var wheel = []color.Color{
@@ -40,15 +16,6 @@ var wheel = []color.Color{
 	color.RGBA{0, 255, 255, 255},
 	color.RGBA{0, 0, 255, 255},
 	color.RGBA{255, 0, 255, 255},
-}
-
-var wheelArray = []int{
-	0xff0000, // {255, 0, 0},
-	0xffff00, // {255, 255, 0},
-	0x00ff00, // {0, 255, 0},
-	0x00ffff, // {0, 255, 255},
-	0x0000ff, // {0, 0, 255},
-	0xff00ff, // {255, 0, 255},
 }
 */
 
@@ -61,13 +28,16 @@ var diffArray = []int{
 	-0x1,     // {0, 0, -1},
 }
 
+// MakePalette contructs a palette
+// of 255*6 colors starting at Red{255,0,0}
+// it des this by walking  the sides of a square
 func MakePalette() []color.RGBA {
 	var a = [3]uint8{}
 	v := 0xff0000 // <--> {255, 0, 0}
 	r := []color.RGBA{}
 	for i := 0; i < 6; i++ {
 		for j := 0; j < 255; j++ {
-			a = base256(v)
+			a = math.Base256(v)
 			r = append(r, color.RGBA{a[0], a[1], a[2], 255})
 			v += diffArray[i]
 		}
@@ -75,76 +45,6 @@ func MakePalette() []color.RGBA {
 	return r
 }
 
-func PxColor3(duration int) color.Color {
-	if duration == 0 { // tookTooLong
-		return color.Black
-	}
-	// iterations = 4*600
-	// const M = 16777215 // 255,255,255 (white)
-	// f := int(math.Floor(float64(M)/float64(iterations)))
-	// d := 2*600  // half
-	w := base256(f * duration) // [23,11,18]
-	// fmt.Println("reversed ",255-w[0],255-w[1],255-w[2])
-	// fmt.Printf("\n%v, %T\n",w,f)
-	// return color.RGBA{w[0], w[1], w[2], 255}
-	return color.RGBA{255 - w[2], 255 - w[1], 255 - w[0], 255}
-}
 
-func PxColor2(duration int) color.Color {
-	if duration == 0 { // tookTooLong
-		return color.Black
-	}
-	m := uint8(duration % 256)
-	// x := 1-float64(duration)/float64(iterations); //  0 < x < 1, closer to 0 for longer ones
-	// m := uint8(math.Floor(255*x)+1)
-	return color.RGBA{255, m, 0, 255}
-	// return color.RGBA{1, 1, 1, 255}
-}
 
-func PxColor1(duration int) color.Color {
-	if duration == 0 { // tookTooLong
-		return color.Black
-	}
-	m := uint8(duration % 256)
-	// x := 1-float64(duration)/float64(iterations); //  0 < x < 1, closer to 0 for longer ones
-	// m := uint8(math.Floor(255*x)+1)
-	return color.RGBA{m, m, m, 255}
-	// return color.RGBA{1, 1, 1, 255}
-}
 
-const T = 8355771 //codeColors([3]int{127,127,127})
-
-// coloRation uses the ratio of z to the total
-// number of iterations modified by a quadratic function
-func coloRatio(z int) uint64 {
-	B := col
-	max := iterations
-	x := float64(z) / float64(max)
-	y := (1 - B*x - (1-B*float64(max))*x*x) * float64(T)
-	return uint64(math.Floor(y))
-}
-
-func codeColors(c [3]int) int {
-	const B = 1 << 8
-	return ((c[0]*B)+c[1])*B + c[2]
-}
-
-func base256(v int) [3]uint8 {
-	const B = 1 << 8 // 256
-	var ww [3]uint8
-	ww[0] = uint8(v >> 16)
-	ww[1] = uint8((v >> 8) % B)
-	ww[2] = uint8(v % B)
-
-	return ww
-}
-
-func getColors(v uint64) [3]uint8 {
-	const B = 1 << 8
-	var w [3]uint8
-	for i := 0; i < 3; i++ {
-		w[2-i] = uint8(v % B)
-		v = v / B
-	}
-	return w
-}
